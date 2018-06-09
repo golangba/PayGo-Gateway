8ce9af78625c9f741c2f26a28c83eb4067ff00b6
Raulino Neto <raulinoneto91@gmail.com>
2018-06-09 17:13:28 -0300

    Creating a accesstoken function

--- gateways/mercadopago/accesstoken/accesstoken.go
+++ gateways/mercadopago/accesstoken/accesstoken.go
@@ -3,13 +3,20 @@ package accesstoken
 import (
 	"encoding/json"
 	"fmt"
+	"PayGo-Gateway2/gateways/helpers/sendrequest"
 )
 
+type resultCredentials struct {
+
+}
 type Credentials struct {
 	GrantType    string `json:"grant_type"`
 	ClientId     string `json:"client_id"`
 	ClientSecret string `json:"client_secret"`
+	AccessToken string `json:"access_token, omnitempty"`
 	url string
+	err error
+	response map[string]interface{}
 }
 func (c Credentials) GetUrl() (string, error) {
 	if len(c.url) == 0{
@@ -23,7 +30,8 @@ func (c Credentials) GetContentType() (string, error) {
 	return "application/json;charset=utf-8", nil
 }
 
-func (c Credentials) GetBody() ([]byte, error) {
+func (c *Credentials) GetBody() ([]byte, error) {
+
 	j, err := json.Marshal(c)
 	if err != nil {
 		return nil, err
@@ -32,14 +40,24 @@ func (c Credentials) GetBody() ([]byte, error) {
 }
 
 func (c *Credentials) SetResponse(b []byte) error {
-	err := json.Unmarshal(b, &t.Response)
+	err := json.Unmarshal(b, &c.response)
 	if err != nil {
 		return err
 	}
 	return nil
 }
 
+func GetAccessToken(url string,c Credentials) (string, error){
+	c.url = url
+	_, err:=sendrequest.SendRequest(&c, "POST")
+	if err != nil {
+		return "", fmt.Errorf("Error: %s", err)
+	}
 
-func GetAccessToken(url string, c Credentials){
+	if m, ok := c.response["message"]; !ok{
+		return "", fmt.Errorf("mercado pago message: %v", m)
+	}
 
+
+	return "", nil
 }
--- gateways/mercadopago/accesstoken/accesstoken_test.go
+++ gateways/mercadopago/accesstoken/accesstoken_test.go
@@ -1,1 +1,26 @@
 package accesstoken
+
+import (
+	"testing"
+	"fmt"
+)
+
+func TestGetAccessToken(t *testing.T) {
+	c := Credentials{
+		GrantType: "client_credentials",
+		ClientId:"7130376828807209",
+		ClientSecret:"ylLotexirqboqPmqgtjOMXWfYOFh3VY4",
+	}
+	url := "https://api.mercadopago.com/oauth/token"
+
+	r, err:= GetAccessToken(url, c)
+	checkTestError(err, t)
+	fmt.Println(r)
+}
+
+func checkTestError(err error, t *testing.T){
+	if err != nil{
+		t.Errorf("Error: %s", err)
+	}
+}
+
